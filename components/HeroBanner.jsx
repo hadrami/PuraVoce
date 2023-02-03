44671dc7c6c9098c3b9e0aa4f5ebf61232295f4f
hadrami <touradhadrami@gmail.com>
2023-02-04 00:26:11 +0100

    Update HeroBanner.jsx

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,46 +1,29 @@
+import React from 'react';
+import Link from 'next/link';
 
-        {/*<div className='flex flex-row  gap-4 md:flex-row sm:flex-row '>
-              <div>
-                <p className="beats-solo">{heroBanner.smallText}</p>
-                <h1>{heroBanner.largeText1}</h1>
-                <h3>{heroBanner.midText}</h3>
-              </div>
-              <div>
-                 <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
-              </div>
-          </div>
-  <div>*/}
-  import React from 'react';
-  import Link from 'next/link';
-  
-  import { urlFor } from '../lib/client';
-  
-  const HeroBanner = ({ heroBanner }) => {
-    return (
-      <div className="hero-banner-container">
+import { urlFor } from '../lib/client';
+
+const HeroBanner = ({ heroBanner }) => {
+  return (
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
+        <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
+
         <div>
-            <div className='grid-container'>
-                  <div className='grid-child'>
-                    <p className="beats-solo">{heroBanner.smallText}</p>
-                    <h1>{heroBanner.largeText1}</h1>
-                    <h3>{heroBanner.midText}</h3>
-                  </div>
-                  <div className='grid-child'>
-                    <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
-                  </div>
-            </div>  
-            <div>
-                <Link href={`/product/${heroBanner.product}`}>
-                  <button type="button">{heroBanner.buttonText}</button>
-                </Link>
-                <div className="desc">
-                  <h5>Description</h5>
-                  <p>{heroBanner.desc}</p>
-                </div>
-            </div>
+          <Link href={`/product/${heroBanner.product}`}>
+            <button type="button">{heroBanner.buttonText}</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>{heroBanner.desc}</p>
+          </div>
         </div>
       </div>
-    )
-  }
-  
-  export default HeroBanner
+    </div>
+  )
+}
+
+export default HeroBanner
